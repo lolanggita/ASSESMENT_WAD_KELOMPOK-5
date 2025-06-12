564fc2d6725cb786ddf9cb7a4edcd9ba8eacc275
Lola Anggita <suchcemeann@gmail.com>
2025-06-12 10:29:39 +0700

    Adding Comments web Controller, Api Controller, models, and migration

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,4 +9,15 @@ class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+    protected $fillable = ['user_id', 'event_id', 'content'];
+
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
+
+    public function event()
+    {
+        return $this->belongsTo(\App\Models\Event::class);
+    }
 }
